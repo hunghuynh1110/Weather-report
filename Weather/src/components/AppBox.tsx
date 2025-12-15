e19881821409bbcc6c3177db2e4484dc5c5127ff
1110giahung <144457830+1110giahung@users.noreply.github.com>
2025-12-15 15:05:38 +0700

    style: reduce padding to 1 and change color of 2nd box template

--- Weather/src/components/AppBox.tsx
+++ Weather/src/components/AppBox.tsx
@@ -2,12 +2,12 @@ export const appBoxSx = {
   borderRadius: 2,
   color: "white",
   bgcolor: "#2f2f49",
-  p: 2,
+  p: 1,
 };
 
 export const appBoxSx2 = {
   borderRadius: 2,
   color: "white",
-  bgcolor: "secondary.main",
-  p: 2,
+  bgcolor: "#2f2f49",
+  p: 1,
 };
