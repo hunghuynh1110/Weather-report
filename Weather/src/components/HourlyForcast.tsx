477e6da7c89865138d174d518f8ae7aac16edefd
1110giahung <144457830+1110giahung@users.noreply.github.com>
2025-12-15 15:07:42 +0700

    feat: draft of hourly forecast, reuse dropdown button from navbar

--- Weather/src/components/HourlyForcast.tsx
+++ Weather/src/components/HourlyForcast.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import type { SelectChangeEvent } from "@mui/material/Select";
+import { appBoxSx } from "./AppBox";
+
+function BasicSelect() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
 
 const HourlyForcast = () => {
-  return <div></div>;
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4">Hourly Forecast</Typography>
+        <BasicSelect />
+      </Box>
+    </Box>
+  );
 };
 
 export default HourlyForcast;
